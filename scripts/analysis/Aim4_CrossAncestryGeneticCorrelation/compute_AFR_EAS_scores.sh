50a27d62677c607912a93596735b9351f1d4bfae
om-jahagirdar <67299586+om-jahagirdar@users.noreply.github.com>
2024-01-10 18:20:17 -0500

    added score computation scripts
    added scripts to compute the cross-ancestry scores between two populations' reference panels

---
+++ scripts/analysis/Aim4_CrossAncestryGeneticCorrelation/compute_AFR_EAS_scores.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --time=4:00:00
+#SBATCH --mail-user=jahagirdarob@nih.gov
+#SBATCH --mail-type=ALL
+#SBATCH --mem=16g
+
+# loads in python and installs Popcorn
+module load python/3.9
+cd /data/Dutta_lab/Popcorn
+
+# populations are numbered alphabetically
+POP1=AFR
+POP2=EAS
+
+# precomputes scores using popcorn
+POP1_REF=/data/Dutta_lab/REF/$POP1
+POP2_REF=/data/Dutta_lab/REF/$POP2
+OUT=/data/Dutta_lab/Popcorn/${POP1}_${POP2}_scores.txt
+
+popcorn compute -v 1 --SNPs_to_store 100000 --bfile1 $POP1_REF --bfile2 $POP2_REF $OUT
